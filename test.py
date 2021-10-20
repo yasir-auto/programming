dc7762127697aa5d2d70a9d8422903624fff30c9
yasir-auto <yasirhussain013@gmail.com.com>
2021-10-20 10:24:40 +0500

    ex1

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+'''
 print (7 * 7 * 24 * 60)
 
 print (25+55)
 
 print (299792458 * 100 * 1/1000000000)
+'''
+
+#Test of Debugging ex-1
+
+print (1)
+print (2)
+print (3)
+print (4)
+print (5)
+print (6)
+print (7)
+print (8)
+print (9)
