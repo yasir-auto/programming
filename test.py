c7369e218b239f2d5ef60883bdaab1650dab88a3
yasir-auto <yasirhussain013@gmail.com.com>
2021-10-20 10:29:01 +0500

    ex2

--- test.py
+++ test.py
@@ -7,13 +7,39 @@ print (299792458 * 100 * 1/1000000000)
 '''
 
 #Test of Debugging ex-1
-
+'''
 print (1)
 print (2)
 print (3)
 print (4)
 print (5)
 print (6)
+print (7)
+print (8)
+print (9)
+'''
+#Test of Debugging ex-2
+'''
+print (1)
+print  (2)
+print   (3)
+print    (4)
+print     (5)
+print       (6)
+print          (7)
+print             (8)
+print                (9)
+'''
+
+#Test of Debugging ex-3
+
+
+print (1)
+print (2)
+print (3)
+print (4)
+print (5)
+Print (6)
 print (7)
 print (8)
 print (9)
