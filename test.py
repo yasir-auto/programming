f4cd46fe7b19cc142fe8227b306bf88a878de2de
yasir-auto <yasirhussain013@gmail.com>
2021-11-08 12:25:13 +0500

    ex22

--- test.py
+++ test.py
@@ -263,4 +263,30 @@ new_sentence += substring3
 print (new_sentence)
 '''
 
+# Variables 1 -ex22
+# Mary is a world class spy with different aliases across the world.
 
+# Mary is known as Randa in America. 
+# But in Europe, her alias Randa has another alias known as Katie.
+# In Asia, the alias Katie has another alias known as Salwa.
+# In Australia, the alias Salwa is known as Kathleen.
+# In South America, the alias Kathleen is known as the alias Gabriel.
+
+# You're a spy yourself and you want to be able to print the real name associated with
+# all of these alias names to keep track of Mary, but you only know that 
+# gabriel = kathleen, and kathleen = salwa, etc..
+
+# Your mission: knowing how each alias relates to each other, assign the variables 
+# gabriel, kathleen, salwa, katie, and randa to each other so whenever we print any alias,
+# the values for each alias will point to the string "Mary"
+
+# NOTE: We can't simply assign all variables to "Mary".
+'''
+mary = "Mary"
+randa = mary
+katie = randa
+salwa = katie
+kathleen = salwa
+gabriel = kathleen
+print(gabriel)
+'''
