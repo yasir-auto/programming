4d2c871eb846f5f729d65eedc5257cf15d41f6f0
yasir-auto <yasirhussain013@gmail.com>
2021-11-08 12:36:47 +0500

    ex23

--- test.py
+++ test.py
@@ -290,3 +290,20 @@ kathleen = salwa
 gabriel = kathleen
 print(gabriel)
 '''
+
+# Finding Strings ex23
+'''
+text = """Wow this is a fairly long body of text. Quite a few characters too.
+I wonder if the string.find method could help find where NOUN is located.
+That way, I could go out and VERB with my friends rather than counting characters
+all day long!"""
+
+noun_position = text.find("NOUN")
+verb_position = text.find("VERB")
+
+print(noun_position)
+print(verb_position)
+'''
+
+
+
