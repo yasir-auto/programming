dbdecf477567c3c805c9b320aec3bca3f6e12fb2
yasir-auto <yasirhussain013@gmail.com>
2021-11-10 11:06:34 +0500

    ex28

--- test.py
+++ test.py
@@ -397,3 +397,32 @@ print (udacify("dacians"))
 print (udacify('turn'))
 print (udacify('boat'))
 '''
+
+# Equality Comparisons ex28
+'''
+print ("Example 1: Greater-than and less-than comparisons")
+print (2 > 1)
+print (1 > 2)
+print (5 < 20)
+print (100 < 19)
+'''
+'''
+print ("Example 2: Equality and non-equality comparisons")
+print(5==5)
+print ("hello"=="hello")
+print(5==10)
+print(5=='5') # what do you think will happen here?
+print(7!=10)
+print(7!=7)
+'''
+'''
+print ("Example 3: A demo of what you are about to learn")
+if 3<5:
+    print("Three is smaller then 5!")
+
+if 10>20:
+    print("10 is gratter then 20!")
+else:
+    print("20 is greater than 10!")
+'''
+
