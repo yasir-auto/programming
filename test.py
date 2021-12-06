cc069d5d2cd85c849e7fd6cddc3f1142fa8334cf
yasir-auto <yasirhussain013@gmail.com>
2021-12-06 17:39:41 +0500

    up

--- test.py
+++ test.py
@@ -869,7 +869,16 @@ nested_list = [['HTML', 'Hypertext Markup Language forms the structure of webpag
                ['Python', 'Python is a programming language'],
                ['Lists', 'Lists are a data structure that let you organize information']]
 
-first_concept = nested_list[0]
+first_concept = nested_list[1]
 
 print ("What do you think this will print?")
 print (first_concept)
+
+
+print ("Since first_concept was itself a list, we can access its elements")
+first_title = first_concept[0]
+first_description = first_concept[1]
+
+print ("What will this print?")
+print (first_title)
+print (first_description)
